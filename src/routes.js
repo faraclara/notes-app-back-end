548504ec0107d4d5c43c296d0e5003eafc737c84
faraclara <faraadevianaaa@gmail.com>
2024-03-24 14:48:46 +0700

    Ubah catatan

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,9 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+const {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+  } = require('./handler');
 
 const routes = [
     {
@@ -16,6 +21,12 @@ const routes = [
         method: 'GET',
         path: '/notes/{id}',
         handler: getNoteByIdHandler,
+      },
+
+      {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
       },
   ];
    
